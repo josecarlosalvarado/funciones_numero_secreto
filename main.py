c120822b68c41989d2e763d2592e4652df2361d5
jose carlos <josecarlos@MacBook-de-Illya.local>
2020-10-28 13:50:58 +0100

    añadido main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from functions import play_game, lista_puntuaciones, lectura_fichero
+
+scores = ...
+jugar = play_game(scores)
+print(jugar)
+
+lista = lista_puntuaciones(scores)
+print(lista)
+
+lectura = lectura_fichero()
+print(lectura)
+
+
+
+
